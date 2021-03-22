922257167ace441c69496d1405999c103cbfc851
mateoaguilarm <mateo.aguilar243@gmail.com>
2021-03-21 22:08:53 -0500

    1st commit

---
+++ 2do QS/2do deber/app.js
@@ -0,0 +1,26 @@
+function normal() {
+    document.getElementById("imagen").className = "nuevoDis";
+  }
+  
+  function filtro1() {
+    document.getElementById("imagen").className = "filtro1";
+  }
+  
+  function filtro2() {
+    document.getElementById("imagen").className = "filtro2";
+  }
+  function filtro3() {
+    document.getElementById("imagen").className = "filtro3";
+  }
+  
+//   const btn1 = document.querySelector('#btn-1')
+  
+//   btn1.addEventListener('click', agrandar)
+  
+//   function agrandar() {
+//     document.getElementById("imagen").className = 'filtro4';
+//   }
+
+  function filtro4() {
+    document.getElementById("imagen").className = 'filtro4';
+  }
---
+++ 2do QS/Canvas/index.js
@@ -0,0 +1,7 @@
+let c = document.getElementById("canvas1");
+let ctx = c.getContext("2d");
+let img = document.getElementById("shape");
+let img2 = document.getElementById("shape2")
+
+ctx.drawImage(img,425,10);
+ctx.drawImage(img2,225,250);
